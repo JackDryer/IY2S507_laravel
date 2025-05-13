f2c8aed1b2426d7da5387177ae96031c19f6d3b5
Jack Dryer <jackjbld@gmail.com>
2025-05-13 16:48:53 +0100

    added login and logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -28,7 +29,17 @@ class AuthController extends Controller
     }
 
     public function login (Request $request){
-        return view ("auth.login");
+            $validated = $request->validate([
+            'email'=> 'required|email|',
+            'password' => 'required|string'
+        ]);
+        if (Auth::attempt($validated)){
+            $request->session()->regenerate();
+            return redirect()->route("home");
+        }
+        throw ValidationException::withMessages([
+            'credentials'=>'Sorry, incorrect credentials'
+        ]);
     }
     public function logout (Request $request){
         Auth::logout();
