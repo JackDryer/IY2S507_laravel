25952d6d476c83b1aba5240adf079c7bd47a4db8
Jack Dryer <jackjbld@gmail.com>
2025-05-17 23:38:52 +0100

    added alpine js

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -7,16 +7,21 @@
                 <th>@sortablelink('colour.colour', 'Colour') </th>
             </tr>
         {{-- This feels like there is an easier way --}}
-        <tbody>
+        <!--tbody-->
             @foreach ($assets as  $asset)
-            <tr>
-            
+                <tbody  x-data="{ open: false }">
+                <tr @click="open = !open">
                     <td class = "custom-list-item"><h3>{{ $asset->name }}</h3></td>
                     <td class = "custom-list-item"><p>{{$asset->colour->colour}} {{$asset->device->name}}</p></td>
-                </div>
-            </tr>
+                </tr>
+                <tr x-show="open" x-transition class = "custom-list-item">
+                    <td colspan="2">CPU: {{$asset->device->cpu->name}}</td>
+                </tr>
+                <!--/tbody-->
             @endforeach
         </tbody>
     </table>
     {!! $assets->appends(\Request::except('page'))->render() !!}
+    <br>
+    <a href ="{{route("user.available_assets")}}" class = "btn">Request an Asset</a>
 </x-layout>
