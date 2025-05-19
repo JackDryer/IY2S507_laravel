8831f7cdaa7c79f69cf2d3d40916798e3c5760fe
Jack Dryer <jackjbld@gmail.com>
2025-05-19 22:47:30 +0100

    fixed a bug where clicking request opened the details

--- resources/views/user/available_assets.blade.php
+++ resources/views/user/available_assets.blade.php
@@ -12,9 +12,9 @@
                 <x-expandable-asset :asset="$asset">
                     <form action="{{route("user.request_asset")}}" method="POST">
                         @csrf
-                        <input type="hidden" name="id" value="{{$asset->id}}">
+                        <input type="hidden" name="id" value="{{$asset->id}} open">
                         <td class="justify-center"> 
-                            <button class="btn block mx-auto">Request</button>
+                            <button class="btn block mx-auto" @click.stop="open">Request</button>
                         </td>
                     </form>
                 </x-expandable-asset>
