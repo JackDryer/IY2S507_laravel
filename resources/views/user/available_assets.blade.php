c236c86077097452bd52c651d52633dbb0793a8f
Jack Dryer <jackjbld@gmail.com>
2025-05-20 00:38:28 +0100

    fixed bug where assests where not requested

--- resources/views/user/available_assets.blade.php
+++ resources/views/user/available_assets.blade.php
@@ -12,7 +12,7 @@
                 <x-expandable-asset :asset="$asset">
                     <form action="{{route("user.request_asset")}}" method="POST">
                         @csrf
-                        <input type="hidden" name="id" value="{{$asset->id}} open">
+                        <input type="hidden" name="id" value="{{$asset->id}}">
                         <td class="justify-center"> 
                             <button class="btn block mx-auto" @click.stop="open">Request</button>
                         </td>
